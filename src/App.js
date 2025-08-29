c83a6720a60e618ef06c6abc6d534a1520c55f3e
rodmig-dev <rodmig2013@gmail.com>
2025-08-28 22:38:51 -0300

    Teste tabuleiro com square

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// Criação do elemento square que irá compor o tabuleiro do jogo da velha
+import './App.css';
+
+export default function Square(){
+  return(
+    <div>
+      <div>
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+   <div>
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+       <div>
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </div>
+  );
+
+}
