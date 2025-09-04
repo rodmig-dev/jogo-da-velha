d943805f5ee17a0c81ebb0a297b63b9f5793f926
rodmig-dev <rodmig2013@gmail.com>
2025-09-04 19:31:49 -0300

    NEW:evento onClick com apresentação do X no square

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 // Criação do elemento square que irá compor o tabuleiro do jogo da velha
+import { useState } from 'react';
 import './App.css';
 
 // Componente Square
-function Square({valor}){
+function Square(){
+  const [valor, setValor] = useState(null)
   function handleClick(){
-    console.log("Botão Clicado!!");
+    setValor('X');
   }
   return(
-    <button className="square" onClick={handleClick}>{valor}</button>
+    <button className="square" onClick={handleClick}>
+      {valor}
+    </button>
   )
 }
 
@@ -15,21 +19,21 @@ export default function Tabuleiro(){
   return(
     <div>
       <div>
-        <Square valor = "1" />
-        <Square valor = "2" />
-        <Square valor = "3" />
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
 
       <div>
-        <Square valor = "4" />
-        <Square valor = "5" />
-        <Square valor = "6" />
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
         
       <div>
-        <Square valor = "7" />
-        <Square valor = "8" />
-        <Square valor = "9" />
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
     </div>
   );
