583082a5828ca039ac884b8e675192d9eeda60d0
rodmig-dev <rodmig2013@gmail.com>
2025-09-04 21:44:18 -0300

    NEW:implementação do vencedor

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function Tabuleiro(){
 
   function handleClick(i){
     const nextSquares = squares.slice();
-    if(nextSquares[i]){
+    if(nextSquares[i] || calculaVencedor(squares)){
       return;
     }
     if (xIsNext){
@@ -58,4 +58,21 @@ export default function Tabuleiro(){
     </div>
   );
 
+}
+
+function calculaVencedor(squares){
+  const lines = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,3], [2,5,8], [0,4,8], [2,4,6]];
+
+  for(let i = 0; i < lines.length; i++){
+
+    const [a,b,c] = lines[i];
+
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+    //else{
+      //return null;
+    //}
+  }
+  return null;
 }
